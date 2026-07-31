9e2ec8db41b28009ebb7123951090bbac54ae9dd
Carlos Rodrigues <crodrigsantos@gmail.com>
2026-07-31 12:22:28 -0300

    test: fix unit test

--- math_test.go
+++ math_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSoma(t *testing.T) {
 	total := soma(10, 10)
 
-	if total != 30 {
+	if total != 20 {
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado %d", total, 30)
 	}
 }
